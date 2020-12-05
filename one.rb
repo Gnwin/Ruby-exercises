8a991942a5e1c9ce9f32f9b0a5b61db6a97f8317
Gnwin <nwgwin@gmail.com>
2020-12-05 17:32:42 +0100

    reading over the exercises

--- one.rb
+++ one.rb
@@ -134,9 +134,6 @@
 			" SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
 			from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>' "
 			
-			"""There is an opening bracket somewhere in the program without a closing bracket following it. It may have happened when creating a hash.""
-				
-
-
-
+			"""There is an opening bracket somewhere in the program without a closing bracket following it. It may have happened when creating a hash."""
+		
 
